63ba9e6804d4bbfe23e13c35dead29abb157482c
张智超 <zhangzhichao@promote.cache-dns.local>
2017-08-26 15:33:29 +0800

    update model

--- movies/models.py
+++ movies/models.py
@@ -4,6 +4,17 @@ from django.db import models
 
 
 class Movies(models.Model):
+    YOUKU = 0
+    SOURCE_TYPE = (
+        (YOUKU, _('youku')),
+    )
+    WHOLE = 0
+    TRAILER = 1
+    FILM_TYPE = (
+        (WHOLE, _('whole film')),
+        (TRAILER, _('Trailer')),
+    )
+
     title = models.CharField(max_length=255)
     url = models.TextField(blank=False)
     bg_img_url = models.TextField(blank=True, null=True)  # 电影海报
@@ -12,13 +23,14 @@ class Movies(models.Model):
     from_at = models.CharField(max_length=100, blank=True, null=True)
     score = models.DecimalField(max_digits=4, decimal_places=1, blank=True, null=True)
     year = models.IntegerField(blank=True, null=True)  # 上映年份
-    op_type = models.CharField(max_length=255, blank=True, null=True)  # 类型
+    op_type = models.CharField(max_length=255, blank=True, null=True)  # 电影类型
+    film_type = models.IntegerField(choices=FILM_TYPE, default=WHOLE)
     tags = models.ManyToManyField('movies.MovieTags')  #
     summary = models.TextField(blank=True, null=True)
     create_at = models.DateTimeField(auto_now=True)
     update_at = models.DateTimeField(auto_now=True, blank=True, null=True)
 
-    source_type = models.IntegerField(blank=True, null=True)
+    source_type = models.IntegerField(choices=SOURCE_TYPE, default=YOUKU, blank=True, null=True)  # 来源
     source_key = models.CharField(max_length=255, blank=True, null=True)
     source_id = models.CharField(max_length=255, blank=True, null=True)
 
--- movies/spider/movies_spider/pipelines.py
+++ movies/spider/movies_spider/pipelines.py
@@ -7,28 +7,85 @@
 
 
 # import redis
-
+import datetime
 from scrapy import signals
-
+import re
 import json
 import codecs
+import logging
 from collections import OrderedDict
 from base.import_django_model import *
+from movies.models import Movies
+
+key_list = ['title', 'url', 'bg_img_url_list']
 
 
 class JsonWithEncodingPipeline(object):
     def __init__(self):
-        self.file = codecs.open('data_utf8.json', 'w', encoding='utf-8')
+        # self.file = codecs.open('data_utf8.json', 'w', encoding='utf-8')
+        pass
 
     def process_item(self, item, spider):
-        line = json.dumps(OrderedDict(item), ensure_ascii=False, sort_keys=False) + "\n"
-        from movies.models import Movies
-        print('count: ', Movies.objects.all().count())
-        self.file.write(line)
+        items = OrderedDict(item)
+
+        # self.file.write(line)
         return item
 
     def close_spider(self, spider):
-        self.file.close()
+        # self.file.close()
+        pass
+
+    @staticmethod
+    def save_item(cls, items):
+        url_list = items['url']
+        bg_img_url_list = items['bg_img_url']
+        title_list = items['title']
+
+        movies_list = []
+        movies_saved = []
+        for i in xrange(len(title_list)):
+            try:
+                title = title_list[i]
+                if title in movies_saved:
+                    continue
+                url = url_list[i]
+                if url.startswith('//'):
+                    url = 'http:%s' % url
+
+                r = re.findall(r'id_([\S]+)\.html', url.split('/')[-1])
+                source_id = ''
+                if r:
+                    source_id = r[0]
+                # 去重
+                is_exists = False
+                if source_id:
+                    m = Movies.objects.filter(source_id=source_id)
+                    if m.exists():
+                        is_exists = True
+                else:
+                    m = Movies.objects.filter(source_id=source_id)
+                    if m.exists():
+                        is_exists = True
+
+                if is_exists:
+                    m = m[0]
+                else:
+                    m = Movies()
+
+                m.title = title
+                m.url = url
+                m.bg_img_url = bg_img_url_list[i]
+                m.update_at = datetime.datetime.now()
+
+                if is_exists:
+                    m.save()
+                else:
+                    movies_list.append(m)
+                movies_saved.append(title)
+            except Exception as e:
+                logging.error(e)
+
+        Movies.objects.bulk_create(movies_list)
 
 
 class RedisPipeline(object):
