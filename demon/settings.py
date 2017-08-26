5343b86db3ef2a2c0e40ec72a07e322cdb9344a6
张智超 <zhangzhichao@promote.cache-dns.local>
2017-08-27 00:48:55 +0800

    change static root

--- demon/settings.py
+++ demon/settings.py
@@ -5,8 +5,6 @@ import os
 # 个人设置
 PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
@@ -50,23 +48,10 @@ MEDIA_ROOT = ''
 # Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = ''
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
-
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_PATH, 'static'),
-)
 
 # List of finder classes that know how to find static files in
 # various locations.
